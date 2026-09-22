553ae493cf19614dbb53cfda18bb77999234d68a
xismy <iye1994@gmail.com>
2026-09-22 10:36:50 +0200

    zsh config reorganization

--- install.zsh
+++ install.zsh
@@ -9,14 +9,12 @@ function create_link() {
 }
 
 script_path=${${(%):-%N}:P:h}
+xdg_config_home=${script_path:h}
 
-mkdir -p $HOME/.local/bin
-create_link $script_path/add_dot_file.zsh $HOME/.local/bin/add_dot_file
-create_link $script_path/xdg_config_home/zsh/.zprofile ~/.zprofile
 
 cd $script_path/xdg_config_home
 for f in ${(p)$(find -type f)}; do
-	target=${script_path:h}/${f#./}
+	target=${xdg_config_home}/${f#./}
 	mkdir -p ${target:h}
 	if [[ -f $target ]]; then
 		echo "$target already exists."
@@ -24,3 +22,7 @@ for f in ${(p)$(find -type f)}; do
 		create_link ${f:P} $target
 	fi
 done
+
+mkdir -p $HOME/.local/bin
+create_link $script_path/add_dot_file.zsh $HOME/.local/bin/add_dot_file
+create_link ${xdg_config_home}/zsh/.init ~/.zshenv
--- xdg_config_home/zsh/.zshenv
+++ xdg_config_home/zsh/.zshenv
@@ -1,4 +1,14 @@
+export XDG_CONFIG_HOME=${${(%):-%N}:h:h}
+export XDG_DATA_HOME="$HOME/.local/share"
+export XDG_CACHE_HOME="$HOME/.cache"
+
+export ZDOTDIR="$XDG_CONFIG_HOME/zsh"
+
 export HISTFILE="$XDG_DATA_HOME/zsh/history"
 export HISTSIZE=10000
 export SAVEHIST=10000
 
+export EDITOR='nvim'
+export VISUAL='nvim'
+
+export PATH=~/.local/bin:$PATH
